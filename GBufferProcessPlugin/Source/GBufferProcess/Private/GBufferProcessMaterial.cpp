5d86cefee4f7070b33597812624d823585924743
Fade Marx <xcaaxc@163.com>
2022-08-16 09:31:02 +0800

    ...

--- GBufferProcessPlugin/Source/GBufferProcess/Private/GBufferProcessMaterial.cpp
+++ GBufferProcessPlugin/Source/GBufferProcess/Private/GBufferProcessMaterial.cpp
@@ -11,9 +11,9 @@
 #include "RenderGraph.h"
 #include "RendererInterface.h"
 
-IMPLEMENT_GLOBAL_SHADER(FGBufferProcessScreenPassVS, "/Plugin/GBufferProcessPlugin/Private/GBufferModityScreenPass.usf", "MainVS", SF_Vertex);
-IMPLEMENT_GLOBAL_SHADER(FClearRectPS, "/Plugin/GBufferProcessPlugin/Private/GBufferModityScreenPass.usf", "ClearPS", SF_Pixel);
-IMPLEMENT_GLOBAL_SHADER(FCopyTexturePS, "/Plugin/GBufferProcessPlugin/Private/GBufferModityScreenPass.usf", "CopyPS", SF_Pixel);
-IMPLEMENT_GLOBAL_SHADER(FRewritePS, "/Plugin/GBufferProcessPlugin/Private/GBufferModityScreenPass.usf", "RewritePS", SF_Pixel);
+IMPLEMENT_GLOBAL_SHADER(FGBufferProcessScreenPassVS, "/Plugin/GBufferProcessPlugin/Private/GBufferProcessScreenPass.usf", "MainVS", SF_Vertex);
+IMPLEMENT_GLOBAL_SHADER(FClearRectPS, "/Plugin/GBufferProcessPlugin/Private/GBufferProcessScreenPass.usf", "ClearPS", SF_Pixel);
+IMPLEMENT_GLOBAL_SHADER(FCopyTexturePS, "/Plugin/GBufferProcessPlugin/Private/GBufferProcessScreenPass.usf", "CopyPS", SF_Pixel);
+IMPLEMENT_GLOBAL_SHADER(FRewritePS, "/Plugin/GBufferProcessPlugin/Private/GBufferProcessScreenPass.usf", "RewritePS", SF_Pixel);
 
-IMPLEMENT_MATERIAL_SHADER_TYPE(, FMaterialGraphRewriteNormalPS, TEXT("/Plugin/GBufferProcessPlugin/Private/GBufferModityTest.usf"), TEXT("RewriteNormalPS"), SF_Pixel);
+IMPLEMENT_MATERIAL_SHADER_TYPE(, FMaterialGraphRewriteNormalPS, TEXT("/Plugin/GBufferProcessPlugin/Private/GBufferProcessTest.usf"), TEXT("RewriteNormalPS"), SF_Pixel);
